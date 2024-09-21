791ffff82da19743ca1667a97424a932f6047c48
Bigotry0 <43312927+Bigotry0@users.noreply.github.com>
2024-09-22 05:56:10 +0800

    Add the PoolObjectCallback interface (#7)
    Used to receive pool events from the pool manager in the pool object, this doesn't break the plugin's current architecture of managing pool objects based on the factory pattern, it's just a small feature added on top of the base factory.
    
    In some cases, we want a specific pool object to have special functionality independent of its parent class, and implementing a proprietary factory class for it is cumbersome and unnecessary behavior. Because he may be used only once. In this case, using an interface is a good choice.
    
    Pool object to implement the interface or not does not affect the current behavior of the pool manager, the factory base class will be in the handling of pool events (OnTakeFromPool, OnReturnToPool, OnChangedStateInPool), to check whether the object implements the PoolObjectCallback interface, only the implementation of the interface of the object Only objects that implement the interface will go to notify the object.

---
+++ Source/PoolManager/Public/PoolObjectCallback.h
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Lim Young
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PoolManagerTypes.h"
+#include "UObject/Interface.h"
+#include "PoolObjectCallback.generated.h"
+
+UINTERFACE()
+class UPoolObjectCallback : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * Provides for the reception of pool manager events from pool objects.
+ * Implement this interface in your pool object class to receive pool manager events.
+ */
+class POOLMANAGER_API IPoolObjectCallback
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 * Called when the object is taken from the pool.If IsNewSpawned is true, the object is newly spawned.
+	 * @param IsNewSpawned If true, the object is newly spawned.
+	 * @param Transform The transform of the object.
+	 */
+	UFUNCTION(BlueprintNativeEvent, Category = "PoolManager")
+	void OnTakeFromPool(bool IsNewSpawned, const FTransform& Transform);
+
+	/**
+	 * Called when the object is returned to the pool.
+	 */
+	UFUNCTION(BlueprintNativeEvent, Category = "PoolManager")
+	void OnReturnToPool();
+
+	/**
+	 * Called when the object is activated or deactivated.
+	 * @param NewState The new state of the object.
+	 */
+	UFUNCTION(BlueprintNativeEvent, Category = "PoolManager")
+	void OnChangedStateInPool(EPoolObjectState NewState);
+};
