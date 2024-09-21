5bea1cbd7b3fefda26aa4525a67be124ec274f16
Bigotry0 <lim.young@foxmail.com>
2024-09-21 19:13:20 +0800

    Add the PoolObjectCallback interface
    Used to receive pool events from the pool manager in the pool object, this doesn't break the plugin's current architecture of managing pool objects based on the factory pattern, it's just a small feature added on top of the base factory.
    
    In some cases, we want a specific pool object to have special functionality independent of its parent class, and implementing a proprietary factory class for it is cumbersome and unnecessary behavior. Because he may be used only once. In this case, using an interface is a good choice.
    
    Pool object to implement the interface or not does not affect the current behavior of the pool manager, the factory base class will be in the handling of pool events (OnTakeFromPool, OnReturnToPool, OnChangedStateInPool), to check whether the object implements the PoolObjectCallback interface, only the implementation of the interface of the object Only objects that implement the interface will go to notify the object.

---
+++ Source/PoolManager/Public/PoolObjectCallback.h
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Lim Young
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PoolManagerTypes.h"
+#include "UObject/Interface.h"
+#include "PoolObjectCallback.generated.h"
+
+UINTERFACE()
+class UPoolObjectCallback : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * Provides for the reception of pool manager events from pool objects.
+ * Implement this interface in your pool object class to receive pool manager events.
+ */
+class POOLMANAGER_API IPoolObjectCallback
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintNativeEvent, Category = "PoolManager")
+	void OnTakeFromPool(const FTransform& Transform);
+
+	UFUNCTION(BlueprintNativeEvent, Category = "PoolManager")
+	void OnReturnToPool();
+
+	UFUNCTION(BlueprintNativeEvent, Category = "PoolManager")
+	void OnChangedStateInPool(EPoolObjectState NewState);
+};
